3936933c9365ff15735d45eb926ff0cb1fbbf587
amoresjan <amoresjan14@gmail.com>
2020-03-14 18:03:44 +0800

    wip
    wip

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,15 +2,9 @@ const midCards = document.querySelectorAll(".table__body__card");
 let modal = document.querySelector(".modal");
 
 midCards.forEach(card => {
-  let checkbox = card.children[0];
+  let button = card.children[0];
 
-  card.addEventListener("click", event => {
-    checkbox.style.transition = "background-color 200ms";
-    checkbox.classList.toggle("check--active");
-  });
-
-  let menu = card.children[6];
-  menu.addEventListener("click", event => {
+  button.addEventListener("click", event => {
     console.log(checkbox.classList);
     console.log("check--active" in checkbox.classList);
     if ("check--active" in checkbox.classList) {
@@ -24,3 +18,4 @@ midCards.forEach(card => {
 document.querySelector(".close").addEventListener("click", event => {
   modal.style.display = "none";
 });
+
