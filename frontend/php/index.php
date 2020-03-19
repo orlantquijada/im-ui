e1c2ad427cea4ebbad0ca06d0764dccd34a4733f
amoresjan <amoresjan14@gmail.com>
2020-03-19 16:53:11 +0800

    wip

--- frontend/php/index.php
+++ frontend/php/index.php
@@ -9,15 +9,14 @@ $sql = "SELECT * FROM medicine";
 $result = mysqli_query($con, $sql);
 $print = "";
 while ($row = mysqli_fetch_array($result)) {
-  if($row['quantity'] == 0){
-    $print .= 
-    "
+  if ($row['quantity'] == 0) {
+    $print .=
+      "
     <div class='table__body__card--unavailable' onclick = 'return unavailable()'>
     ";
-  }
-  else{
-    $print.=
-    "<div class='table__body__card' onclick = 'return displayModal(" . $row['id'] . ")'>
+  } else {
+    $print .=
+      "<div class='table__body__card' onclick = 'return displayModal(" . $row['id'] . ")'>
     ";
   }
   $print .=
@@ -101,26 +100,96 @@ while ($row = mysqli_fetch_array($result)) {
           echo $print;
           ?>
         </section>
-      </div>
-      <section class='cart'>
-        <input type='submit' value='pay'>
+    </section>
+    <section class="right-side-bar">
+      <section class="right-side-bar__top">
+        <div class="right-side-bar__top__pharmacy">
+          <div class="right-side-bar__top__pharmacy__hero">
+            <img src="../static/hygeia.svg" class="logo" />
+            <h1 class="name">Pharmacy</h1>
+          </div>
+          <h3 class="right-side-bar__top__pharmacy__location">
+            21 St. This Barangay, That City
+          </h3>
+        </div>
+        <div class="line"></div>
+      </section>
+      <section class="right-side-bar__mid">
+        <div class="right-side-bar__mid__transaction">
+          <h1 class="name">Transaction:</h1>
+          <h1 class="number">817231987237</h1>
+        </div>
+        <div class="line"></div>
+      </section>
+
+      <section class="right-side-bar__bot">
+        <section class="table">
+          <div class="table__card">
+            <div class="table__card__item name">
+              <h1>BIOGESIC TABLET 250MG</h1>
+            </div>
+            <div class="table__card__item piece">
+              <h1>5PC</h1>
+            </div>
+            <div class="table__card__item price">
+              <h1>6.00</h1>
+            </div>
+            <div class="table__card__item total">
+              <h1>30.00</h1>
+            </div>
+          </div>
+        </section>
+
+        <section class="calculations">
+          <div class="total-items">
+            <h1 class="name">TOTAL ITEMS:</h1>
+            <h2 class="number">8</h2>
+          </div>
+          <div class="subtotal">
+            <h1 class="name">SUBTOTAL:</h1>
+            <h2 class="number">228.00</h2>
+          </div>
+        </section>
+        <div class="line--broken"></div>
+        <section class="total">
+          <h1 class="name">TOTAL:</h1>
+          <h1 class="number">200.00</h1>
+        </section>
+        <?php
+        $con = mysqli_connect("localhost", "root", "", "pharmacy_db");
+        $sql = "SELECT * FROM transaction WHERE is_payed='false'";
+        $result = mysqli_query($con, $sql);
+        $row = mysqli_fetch_array($result);
+        $transaction="";
+        if($row == NULL){
+          $sql = "INSERT INTO `transaction` (`datetime_purchased`,`employee_id`, `is_payed`) VALUES (getDate(),'1', '0');";
+          
+        }
+        ?>
+        <div class='pay-section'>
+          <input type='submit' value='pay'>
+        </div>
       </section>
     </section>
+  </div>
+  </section>
 
-    <!-- Modal Section -->
-    <?php
-    $con = mysqli_connect("localhost", "root", "", "pharmacy_db");
-    $sql = "SELECT * FROM medicine";
-    $result = mysqli_query($con, $sql);
-    $modal = "";
-    while ($row = mysqli_fetch_array($result)) {
-      $modal .=
-        "
+
+
+  <!-- Modal Section -->
+  <?php
+  $con = mysqli_connect("localhost", "root", "", "pharmacy_db");
+  $sql = "SELECT * FROM medicine";
+  $result = mysqli_query($con, $sql);
+  $modal = "";
+  while ($row = mysqli_fetch_array($result)) {
+    $modal .=
+      "
               <div class='modal' id='" . $row['id'] . "'>
               <div class='add-medicine'>
                 <span class='close'>+</span>
                   <form action='#' method='GET' class='table'>
-                    <input type='text' name = 'id' value='".$row['id']."' style='display:none'>
+                    <input type='text' name = 'id' value='" . $row['id'] . "' style='display:none'>
                     <div class='table__item generic'>
                       <h1 class='name'>Generic Name:</h1>
                       <h1 class='title'>" . $row['generic_name'] . "</h1>
@@ -172,21 +241,22 @@ while ($row = mysqli_fetch_array($result)) {
                 </div>
             </div>
               ";
-    }
-    echo $modal;
+  }
+  echo $modal;
 
-    if(isset($_POST['purchase'])){
-      if(!empty($_POST['qty'])){
-        $quantity = $_POST['quantity'];
-      }
+  if (isset($_POST['purchase'])) {
+    if (!empty($_POST['qty'])) {
+      $quantity = $_POST['quantity'];
     }
-    ?>
+  }
+  ?>
 
   </div>
   </div>
 
   <script language="Javascript">
     let modal = document.querySelector(".modal");
+
     function displayModal(e) {
       document.getElementById(e).style.display = "flex";
     }
@@ -195,7 +265,7 @@ while ($row = mysqli_fetch_array($result)) {
       modal.style.display = "none";
     });
 
-    function unavailable(){
+    function unavailable() {
       alert("Item unavailable!");
     }
   </script>
